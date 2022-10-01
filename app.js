f18c9e3c918d88ca67cb69080c253137b3bb8c2f
ericburger <eric.burger@comcast.net>
2022-10-01 15:14:16 -0500

    Fix missing references

--- app.js
+++ app.js
@@ -2,7 +2,13 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 const http = require('http');
+const path = require('path');
+const cookieParser = require('cookie-parser');
 const sockets = require('./sockets');
+const logger = require('morgan');
+
+const indexRouter = require('./routes/index');
+const lobbiesRouter = require('./routes/lobbies');
 
 sockets.setupSocketConnection(app);
 
@@ -13,7 +19,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/lobbies', lobbiesRouter);
 
 // error handler
--- routes/lobbies.js
+++ routes/lobbies.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 var sockets = require('../sockets');
-const { route } = require('./users');
 
 /* GET lobbies listing. */
 router.get('/', function (req, res, next) {
