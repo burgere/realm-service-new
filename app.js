eb7667a94277c8fc574e81997554455582386893
ericburger <eric.burger@comcast.net>
2022-10-01 15:02:03 -0500

    Add endpoints

--- app.js
+++ app.js
@@ -3,5 +3,27 @@ const app = express();
 const port = process.env.PORT || 3001;
 const http = require('http');
 
+sockets.setupSocketConnection(app);
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/lobbies', lobbiesRouter);
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+});
+
 app.listen(port, () => console.log(`Realm multiplayer service listening on port ${port}!`));
 
